15968c60afc20fbf8bf5415051107cf4d5c7689e
Ely <ely.ashzyl@gmail.com>
2026-08-03 11:41:55 +0800

    Added 1 XP per message sent in messenger

--- app/Http/Controllers/MessengerController.php
+++ app/Http/Controllers/MessengerController.php
@@ -160,6 +160,18 @@ class MessengerController extends Controller
         $conversation->messages()->create(['sender_id' => $user->id, 'body' => $validated['body']]);
         $conversation->touch();
 
+        // Award 1 XP per message sent
+        $user->increment('total_points', 1);
+        \App\Models\StudentPoint::create([
+            'student_id' => $user->id,
+            'activity_type' => 'Message',
+            'activity_id' => 0,
+            'points' => 1,
+            'score' => 1,
+            'total' => 1,
+            'reason' => 'Message sent',
+        ]);
+
         return redirect()->route($user->isStudent() ? 'student.messenger.show' : 'teacher.messenger.show', $conversation);
     }
 
